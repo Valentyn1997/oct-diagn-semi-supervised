Bug fixes, add f1-score calculation

src/data/dataset_plugins.py
@@ -218,10 +218,10 @@ class SSLDatasetPlugin(TorchvisionDatasetPlugin):
 
         labels = None
         if hasattr(train_set, 'labels'):
-            labels = train_set.labels
+            labels = np.array(train_set.labels)
             labels_attr = 'labels'
         elif hasattr(train_set, 'targets'):
-            labels = train_set.targets
+            labels = np.array(train_set.targets)
             labels_attr = 'targets'
 
         uniques = sorted(np.unique(labels).tolist())
@@ -263,6 +263,10 @@ class SSLDatasetPlugin(TorchvisionDatasetPlugin):
 
     def _handle_ImageFolder(self, Dataset, data_path, transform=None, test_transform=None, **kwargs):
         train_set = Dataset(f'{data_path}/train', transform=transform)
+        if 'UNL' in train_set.classes:
+            train_set.targets = np.array(train_set.targets)
+            train_set.targets[train_set.targets == train_set.class_to_idx['UNL']] = -1
+            train_set.class_to_idx['UNL'] = -1
         val_set = Dataset(f'{data_path}/val', transform=test_transform)
         test_set = Dataset(f'{data_path}/test', transform=test_transform)
         return train_set, val_set, test_set

src/models/mix_match/controller.py
@@ -7,9 +7,8 @@ import torch.nn.functional as F
 import numpy as np
 from torchsummary import summary
 
-from src.models.mix_match.utils import calculate_hash
 from src.models.mix_match.utils import interleave
-from src.models.utils import accuracy, MlflowLogger, WeightEMA, EarlyStopping
+from src.models.utils import accuracy, MlflowLogger, WeightEMA, EarlyStopping, f1_score
 from src.models.wideresnet import WideResNet_50_2
 
 
@@ -25,7 +24,7 @@ class MixMatchController(ModelPlugin):
         super().optimizer_step(retain_graph)
         self.ema_optimizer.step()
 
-    def routine(self, T: float = 0.5, alpha: float = 0.9, *args, **kwargs):
+    def routine(self, T: float = 0.5, alpha: float = 0.75, *args, **kwargs):
         """
         :param alpha: Parameter of beta distribution
         :param T: Sharpening temperature
@@ -113,7 +112,11 @@ class MixMatchController(ModelPlugin):
             # top5 = accuracy(outputs_l, targets_l, labeled, top=5)
             self.add_results(acc_top1=top1)
 
-    def build(self, lambda_u: float = 12.5, ema_decay: float = 0.999, early_stopping_patience: int = 250,
+            # F1-score
+            f1 = f1_score(outputs_l, targets_l)
+            self.add_results(f1_score=f1)
+
+    def build(self, lambda_u: float = 12.5, ema_decay: float = 0.999, early_stopping_patience: int = 500,
               run_hash=None, log_to_mlflow=True, type_of_run=None, *args, **kwargs):
         """
         :param early_stopping_patience: Patience for early stopping, number of epochs
@@ -165,17 +168,34 @@ class MixMatchController(ModelPlugin):
         self.early_stopping.on_epoch_end()
 
         # Mlflow Logging
-        MlflowLogger.log_all_metrics('train', self.early_stopping.stopped_epoch)
-        MlflowLogger.log_all_metrics('val', self.early_stopping.stopped_epoch)
-        MlflowLogger.log_all_metrics('test', self.early_stopping.stopped_epoch)
+        if exp.INFO['epoch'] >= exp.ARGS['train']['epochs']:
+            epoch_to_log = self.early_stopping.best_epoch  # Last epoch - logging the best epoch (based on val)
+        elif self.early_stopping.stopped_epoch is not None:
+            epoch_to_log = self.early_stopping.stopped_epoch  # Stopped learning - logging the best epoch (based on val)
+        else:
+            epoch_to_log = exp.INFO['epoch']  # Logging current epoch
+
+        MlflowLogger.log_all_metrics('train', epoch_to_log)
+        MlflowLogger.log_all_metrics('val', epoch_to_log)
+        MlflowLogger.log_all_metrics('test', epoch_to_log)
 
         if self.early_stopping.stopped_epoch is not None:
             mlflow.log_metric('stopped_epoch', self.early_stopping.stopped_epoch, step=exp.INFO['epoch'])
             exp.INFO['epoch'] = exp.ARGS['train']['epochs']
 
     def visualize(self):
-        inputs = self.inputs('data.images')
-        targets = self.inputs('data.targets')
+        if exp.ARGS['data']['data']['split_labelled_and_unlabelled'] and self.data.mode == 'train':
+            inputs_l = self.inputs('data_l.images')
+            targets_l = self.inputs('data_l.targets')
+
+            inputs_u = self.inputs('data_u.images')[0]
+            targets_u = torch.full(targets_l.shape, -1, dtype=torch.long).to(targets_l.device)
+
+            inputs = torch.cat([inputs_l, inputs_u])
+            targets = torch.cat([targets_l, targets_u])
+        else:
+            inputs = self.inputs('data.images')
+            targets = self.inputs('data.targets')
 
         inputs = inputs[:, :1, :, :]
         inputs *= 0.229

src/models/utils.py
@@ -90,6 +90,7 @@ class EarlyStopping(object):
         self.patience = patience
         self.wait = 0
         self.best_value = 0.0
+        self.best_epoch = 0
         self.stopped_epoch = None
 
     def on_epoch_end(self):
@@ -97,17 +98,17 @@ class EarlyStopping(object):
         if current_value is None:
             pass
         else:
-            if (current_value - self.best_value) > self.min_delta:
+            if (current_value - self.best_value) >= self.min_delta:
                 self.best_value = current_value
+                self.best_epoch = exp.INFO['epoch']
                 self.wait = 1
             else:
                 if self.wait >= self.patience:
-                    self.stopped_epoch = exp.INFO['epoch']
+                    self.stopped_epoch = self.best_epoch
                 self.wait += 1
 
 
-
-def accuracy(outputs, targets, labeled, top: int = 1, is_argmax=False):
+def accuracy(outputs: torch.Tensor, targets: torch.Tensor, labeled, top: int = 1, is_argmax=False):
     """Computes the accuracy.
 
     Args:
@@ -129,3 +130,40 @@ def accuracy(outputs, targets, labeled, top: int = 1, is_argmax=False):
         correct_k = correct[:top].view(-1).float().sum(0, keepdim=True)
         accuracy = correct_k.mul_(100.0 / labeled.float().sum())
         return accuracy.detach().item()
+
+
+def f1_score(outputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+    '''Calculate F1 score. Can work with gpu tensors
+
+    The original implmentation is written by Michal Haltuf on Kaggle.
+
+    Returns
+    -------
+    torch.Tensor
+        `ndim` == 1. 0 <= val <= 1
+
+    Reference
+    ---------
+    - https://www.kaggle.com/rejpalcz/best-loss-function-for-f1-score-metric
+    - https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html#sklearn.metrics.f1_score
+    - https://discuss.pytorch.org/t/calculating-precision-recall-and-f1-score-in-case-of-multi-label-classification/28265/6
+
+    '''
+    assert targets.ndim == 1
+    assert outputs.ndim == 1 or outputs.ndim == 2
+
+    if outputs.ndim == 2:
+        outputs = outputs.argmax(dim=1)
+
+    tp = (targets * outputs).sum().to(torch.float32)
+    tn = ((1 - targets) * (1 - outputs)).sum().to(torch.float32)
+    fp = ((1 - targets) * outputs).sum().to(torch.float32)
+    fn = (targets * (1 - outputs)).sum().to(torch.float32)
+
+    epsilon = 1e-7
+
+    precision = tp / (tp + fp + epsilon)
+    recall = tp / (tp + fn + epsilon)
+
+    f1 = 2 * (precision * recall) / (precision + recall + epsilon)
+    return f1.detach().item()

src/models/wideresnet.py
@@ -11,8 +11,6 @@ class WideResNet_50_2(nn.Module):
         self.wrn = torch.hub.load('pytorch/vision:v0.5.0', 'wide_resnet50_2', pretrained=pretrained)
         self.wrn.eval()
         self.wrn.fc = nn.Linear(self.wrn.fc.in_features, num_classes)
-
-    def forward(self, x):
         if self.freeze_layers:
             for name, param in self.wrn.named_parameters():
                 if name == 'fc.weight' or name == 'fc.bias':
@@ -20,5 +18,6 @@ class WideResNet_50_2(nn.Module):
                 else:
                     param.requires_grad = False
 
+    def forward(self, x):
         return self.wrn(x)
 